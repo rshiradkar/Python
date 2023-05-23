15f4aed99a6093acadc118bcceb5b588563101dc
rshiradkar <68760842+rshiradkar@users.noreply.github.com>
2023-05-22 22:55:28 -0400

    Update dicomAnon.py

--- dicomAnon.py
+++ dicomAnon.py
@@ -6,14 +6,20 @@ from dicomanonymizer import anonymize_dicom_file
 import matplotlib.pyplot as plt
 from collections import defaultdict
 
-data_folder = Path("C:/temp/data/patient1")
-data_op_folder = Path("C:/temp/anon_data")
+
+data_folder = Path("C:/Users/rshirad/OneDrive - Emory University/Old Files/CTvsMRI")
+data_op_folder = Path("C:/Users/rshirad/OneDrive - Emory University/Old Files/Anonymized/CTvsMRI_anon")
+
 
 files = os.listdir(data_folder)
 for file in files:
     if file[-4:] == '.dcm':
         filePath = data_folder / file
-        dcmFile = pd.dcmread(filePath)
+        file_op_name = file[:-4] + '_anon.dcm'
+        file_op_path = data_op_folder / file_op_name
+        anonymize_dicom_file(filePath,file_op_path)
+        dcmFile = pd.dcmread(file_op_path)
+
         img = dcmFile.pixel_array
         nRows, nCols = img.shape
         tagName = file + ".tag"
@@ -31,14 +37,15 @@ for file in files:
             for i in range(nRows):
                 for j in range(nCols):
                     if ord(byte) != 0:
-                        mask[j][i] = ord(byte)
+                        mask[i][j] = ord(byte)
                     byte = f.read(1)
 
         # plt.imshow(mask, cmap=plt.cm.gray)
         # plt.colorbar()
         # plt.show()
-        dcmFile.PixelData = mask.tobytes()
-        dcmFile.save_as(data_op_folder / 'out.dcm')
+        dcmFile.PixelData = mask.astype(np.float16).tobytes()
+        maskFileName = file[:-4] + "_anon_mask.dcm"
+        dcmFile.save_as(data_op_folder / maskFileName)
     
 # print(type(mask))
 # print(type(dcmFile.pixel_array))
